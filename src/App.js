1e8f1274b26d2afebc2b445f7924037f143bc8e8
whyyor <whyyor@gmail.com>
2022-02-05 14:21:25 +0530

    created cyberpunk clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import CollectionCard from './components/CollectionCard';
+import { useState,useEffect } from 'react'
+import axios from 'axios';
+import PunkList from './components/PunkList';
 
 function App() {
+  const [punkListData,setPunkListData] = useState([])
+  const [selectedPunk,setSelectedPunk] = useState(0)
+  useEffect(()=>{
+    const getMyNfts = async () => {
+        const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x692D6f9fdd89e44E0225c3EA27925Ea552378a0f&order_direction=asc')
+        console.log(openseaData.data.assets);
+        setPunkListData(openseaData.data.assets)
+    }
+    return getMyNfts()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <Header/>
+        {
+            punkListData.length > 0 && (
+                <>
+                <Main selectedPunk={selectedPunk} punkListData={punkListData} />
+                <PunkList punkListData={punkListData} setSelectedPunk={setSelectedPunk} />
+                </>
+            )}
     </div>
   );
 }
---
+++ src/components/PunkList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import CollectionCard from './CollectionCard'
+import './PunkList.css'
+
+const PunkList = ({punkListData,setSelectedPunk}) => {
+    return (
+        <div className='punkList'>
+            {punkListData.map(punk => (
+                <div onClick={() => setSelectedPunk(punk.token_id)}>
+                    <CollectionCard
+                        key={punk.token_id}
+                        name={punk.name}
+                        traits={punk.traits}
+                        image={punk.image_original_url}
+                    />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default PunkList
